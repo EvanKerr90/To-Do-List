d5dfa0d64c1931322ba932bc176f098f89922b00
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-06-26 21:50:34 -0700

    Added request package and api key

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = '4d9dd3f2346379459d28284bbed32c93';
+
 const app = express();
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended:true}));
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
 })
 
 app.listen(3000, function () {
